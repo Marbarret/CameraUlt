dae3c4b88f8ff1cccf6e996f7fd2321f49072876
Marbarret <marsouza93@gmail.com>
2021-12-23 22:29:35 -0300

    initial
    primeiro commit

--- CameraUlt/ViewController.swift
+++ CameraUlt/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageUser: UIImageView!
+    @IBOutlet weak var addImageButton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.imagePicker.delegate = self
+        
+        self.addImageButton.layer.cornerRadius = 20
+        self.addImageButton.clipsToBounds =  true
     }
 
+    @IBAction func handlerButtonAdd(_ sender: Any) {
+        
+        self.imagePicker.sourceType = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+        
+    }
+}
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imagemRecuperada: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageUser.image = imagemRecuperada
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
